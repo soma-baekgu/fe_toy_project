21a035842fc70897e828832670d5bffda50f6ac8
Dltmd202 <dltmd202@gmail.com>
2024-04-26 12:26:38 +0900

    feat: Link workspace path

--- src/app/seonghwan/components/workspace/WorkspaceButton.tsx
+++ src/app/seonghwan/components/workspace/WorkspaceButton.tsx
@@ -1,16 +1,16 @@
 import Button from "@/app/seonghwan/components/styledButton";
 import styled from "styled-components";
 import {Workspace} from "@/app/seonghwan/components/workspace/Workspace-info";
+import Link from "next/link";
 
 export const WorkspaceButton = ({workspace}: {workspace: Workspace}) => {
-  const onClick = () => {
-
-  }
 
   return (
-    <StyledWorkspaceButton onClick={onClick}>
-      {workspace.name.substring(0, 2)}
-    </StyledWorkspaceButton>
+    <Link href={`seonghwan/${workspace.path}`}>
+      <StyledWorkspaceButton>
+        {workspace.name.substring(0, 2)}
+      </StyledWorkspaceButton>
+    </Link>
   )
 }
 
